494d5648b7e9f0b20c95d07570b31093888341ca
smammc <smammc@hotmail.com>
2024-10-09 16:42:40 +0100

    fixed bug in middleware authentication

--- server/middleware/jwt.middleware.js
+++ server/middleware/jwt.middleware.js
@@ -1,16 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 const isAuthenticated = (req, res, next) => {
+  // console.log(req);
+  if (!req.headers.authorization) {
+    return res.status(401).json({ message: "No headers provided" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Token not provided" });
+  }
   try {
-    if (!req.headers.authorization) {
-      return res.status(401).json({ message: "No headers provided" });
-    }
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
 
-    const token = req.headers.authorization.split(" ")[1];
-    if (!token) {
-      return res.status(401).json({ message: "Token not provided" });
-    }
-
+    // Attach the payload (decoded token data) to the request object
     req.payload = payload;
     next();
   } catch (error) {
@@ -18,31 +21,3 @@ const isAuthenticated = (req, res, next) => {
   }
 };
 module.exports = isAuthenticated;
-
-/* // Instantiate the JWT token validation middleware
-const isAuthenticated = jwt({
-  secret: process.env.TOKEN_SECRET,
-  algorithms: ["HS256"],
-  requestProperty: "payload",
-  getToken: getTokenFromHeaders,
-});
-
-// Function used to extract the JWT token from the request's 'Authorization' Headers
-function getTokenFromHeaders(req) {
-  // Check if the token is available on the request Headers
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[0] === "Bearer"
-  ) {
-    // Get the encoded token string and return it
-    const token = req.headers.authorization.split(" ")[1];
-    return token;
-  }
-
-  return null;
-}
-
-// Export the middleware so that we can use it to create protected routes
-module.exports = {
-  isAuthenticated,
-}; */
