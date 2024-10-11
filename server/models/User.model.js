5363b7d93e1b91384d2d5d42cc3aa2d9d011955c
smammc <smammc@hotmail.com>
2024-10-11 12:58:10 +0100

    modified user model

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -18,6 +18,10 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Name is required."],
     },
+    picture: {
+      type: String,
+      required: false,
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
