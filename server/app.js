74d5ec7f4d5302f644f77b7bf87c65bf9a360a93
smammc <smammc@hotmail.com>
2024-10-24 17:57:10 +0100

    Server modifications to get and update user information

--- server/app.js
+++ server/app.js
@@ -9,20 +9,29 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
+// ==================== Body Parser Setup ====================
+// Increase the size limit for JSON requests
+app.use(bodyParser.json({ limit: "10mb" })); // Set limit for JSON bodies
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true })); // Set limit for URL-encoded bodies
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+// const indexRoutes = require("./routes/index.routes");
+// app.use("/api", indexRoutes);
 
 const todoRoutes = require("./routes/todo.routes");
 app.use("/api", todoRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
+
+const userRoutes = require("./routes/user.routes");
+app.use("/user", userRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
